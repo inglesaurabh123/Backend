02c2d4e19448340d14bf4b0193f13e42b75d9e9f
saurabh shankar ingle <156014019+inglesaurabh123@users.noreply.github.com>
2025-06-04 19:38:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const authRoutes = require("./routes/authRoutes");
 const bookRoutes = require("./routes/bookRoutes");
